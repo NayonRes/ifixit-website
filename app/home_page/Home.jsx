77d822c4ddf2ab89f4c6605e69fff77d67cfebd3
mozammel-newroz <mozammel@newroztech.com>
2024-10-07 00:33:33 +0600

    working on home page section two

--- app/home_page/Home.jsx
+++ app/home_page/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './Banner'
 import Service from './Service'
+import SectionTwo from './SectionTwo'
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <Service />
+      <SectionTwo />
     </div>
   )
 }
--- app/home_page/Service.jsx
+++ app/home_page/Service.jsx
@@ -1,11 +1,11 @@
-import { Box, Container, Grid2, Typography } from "@mui/material";
+import { Box, Container,  Typography } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import React from "react";
 import CardOne from "../components/CardOne";
 
 const Service = () => {
   return (
-    <Container maxWidth="xl" sx={{ my:4 }} >
+    <Container maxWidth="xl" sx={{ py:10 }} >
       <Box sx={{display: 'flex', flexDirection: 'column', gap: 1, mb:6 }} >
       <Typography variant="body1" color="primary">
         SERVICES
