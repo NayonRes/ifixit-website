afcabf366626acb5d4bea3235646093a76b849cd
Mozammel <mozammel@localhost.localdomain>
2024-11-20 12:42:18 +0600

    working on login page

--- app/(pages)/login/page.jsx
+++ app/(pages)/login/page.jsx
@@ -1,21 +1,28 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 const style = {
   outer: {
     background: "url('/bg-sm.png')",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+
     height: "90vh",
     backgroundSize: "container",
-    backgroundPosition: "center",
+    backgroundPosition: "top",
     backgroundRepeat: "no-repeat",
     padding: "20px",
-    width: '100%'
+    width: "100%",
+    paddingTop: { sm: "100px", md: "150px" },
   },
   wrapper: {
-    maxWidth: "650px",
+    maxWidth: "400px",
     mx: "auto",
     display: "flex",
     flexDirection: "column",
@@ -28,6 +35,30 @@ const style = {
     alignItems: "center",
     gap: 2,
     width: "100%",
+  },
+  remember: {
+    display: "flex",
+    gap: 1,
+
+    alignItems: "center",
+    fontSize: 14,
+  },
+  social_button: {
+    height: "45px",
+    border: "1px solid #ddd",
+    color: "#333",
+  },
+  social_button2: {
+    height: "45px",
+    border: "1px solid #000",
+    color: "#fff",
+    background: "#000",
+  },
+  social_button_inner: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: "10px",
   },
   label: { fontSize: 14, fontWeight: 500, mb: 1, color: "#555" },
   input: {
@@ -69,10 +100,46 @@ const page = () => {
               sx={style.input}
             />
           </Box>
+          <Box sx={style.remember}>
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label="Remember for 30 days"
+              sx={{ "& .MuiFormControlLabel-label": { fontSize: 14 } }}
+            />
+            <Link href="#" sx={{ textDecoration: "none", fontWeight: 500 }}>
+              Forgot Password
+            </Link>
+          </Box>
           <Box>
             <Button fullWidth variant="contained" sx={{ height: "45px" }}>
               Sign in
             </Button>
+          </Box>
+          <Box>
+            <Button fullWidth variant="outlined" sx={style.social_button}>
+              <Box sx={style.social_button_inner}>
+                <img src="/icons/google.png" alt="" /> Sign in with Google
+              </Box>
+            </Button>
+          </Box>
+          <Box>
+            <Button fullWidth variant="outlined" sx={style.social_button2}>
+              <Box sx={style.social_button_inner}>
+                <img src="/icons/apple.png" alt="" /> Sign in with Apple
+              </Box>
+            </Button>
+          </Box>
+          <Box
+            sx={{ fontSize: 14, textAlign: "center", mt: 2 }}
+            color="text.light"
+          >
+            Don’t have an account?
+            <Link
+              href="#"
+              sx={{ textDecoration: "none", fontWeight: 500, ml: 1 }}
+            >
+              Sign up
+            </Link>
           </Box>
         </Box>
       </Box>
